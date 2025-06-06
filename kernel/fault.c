42d7eb93b2827340fdefe53d2e300a04fd494c8b
balazs <113060109+balazsmonor1@users.noreply.github.com>
2025-06-06 05:29:19 +0100

    proper fault handler

--- kernel/fault.c
+++ kernel/fault.c
@@ -5,23 +5,87 @@
 
 void fault_handler(int vector) {
     printf("INT #%d RAISED\n", vector);
-    switch (vector) {
+
+    switch ((isr_vector_t)vector) {
         case ISR_DIVIDE_ERROR: 
-            kernel_panic("exception - divide by zero");
+            kernel_panic("exception - divide by zero (#DE)");
+            break;
+        case ISR_DEBUG:
+            kernel_panic("exception - debug (#DB)");
+            break;
+        case ISR_NON_MASKABLE_INT:
+            kernel_panic("exception - non-maskable interrupt (NMI)");
+            break;
+        case ISR_BREAKPOINT:
+            kernel_panic("exception - breakpoint (#BP)");
+            break;
+        case ISR_OVERFLOW:
+            kernel_panic("exception - overflow (#OF)");
+            break;
+        case ISR_BOUND_RANGE_EXCEEDED:
+            kernel_panic("exception - bound range exceeded (#BR)");
             break;
         case ISR_INVALID_OPCODE: 
-            kernel_panic("exception - invalid opcode"); 
+            kernel_panic("exception - invalid opcode (#UD)"); 
+            break;
+        case ISR_DEVICE_NOT_AVAILABLE:
+            kernel_panic("exception - device not available (#NM)");
+            break;
+        case ISR_DOUBLE_FAULT:
+            kernel_panic("exception - double fault (#DF)");
+            break;
+        case ISR_COPROC_SEG_OVERRUN:
+            kernel_panic("exception - coprocessor segment overrun");
+            break;
+        case ISR_INVALID_TSS:
+            kernel_panic("exception - invalid TSS (#TS)");
+            break;
+        case ISR_SEG_NOT_PRESENT:
+            kernel_panic("exception - segment not present (#NP)");
+            break;
+        case ISR_STACK_SEG_FAULT:
+            kernel_panic("exception - stack segment fault (#SS)");
             break;
         case ISR_GENERAL_PROTECTION: 
-            kernel_panic("exception - general protection fault"); 
+            kernel_panic("exception - general protection fault (#GP)"); 
             break;
         case ISR_PAGE_FAULT: 
-            kernel_panic("exception - page fault"); 
+            kernel_panic("exception - page fault (#PF)"); 
             break;
+        case ISR_FPU_ERROR:
+            kernel_panic("exception - x87 floating point error (#MF)");
+            break;
+        case ISR_ALIGNMENT_CHECK:
+            kernel_panic("exception - alignment check (#AC)");
+            break;
+        case ISR_MACHINE_CHECK:
+            kernel_panic("exception - machine check (#MC)");
+            break;
+        case ISR_SIMD_FP_EXCEPTION:
+            kernel_panic("exception - SIMD floating-point exception (#XF)");
+            break;
+        case ISR_VIRTUALIZATION:
+            kernel_panic("exception - virtualization exception");
+            break;
+
+        case ISR_RESERVED_15:
+        case ISR_RESERVED_21:
+        case ISR_RESERVED_22:
+        case ISR_RESERVED_23:
+        case ISR_RESERVED_24:
+        case ISR_RESERVED_25:
+        case ISR_RESERVED_26:
+        case ISR_RESERVED_27:
+        case ISR_RESERVED_28:
+        case ISR_RESERVED_29:
+        case ISR_RESERVED_30:
+        case ISR_RESERVED_31:
+            kernel_panic("exception - reserved/unknown CPU exception");
+            break;
+
         default: 
-            kernel_panic("exception - pnknown exception"); 
+            kernel_panic("exception - unknown interrupt vector"); 
             break;
     }
-
-    abort();
 }
+
