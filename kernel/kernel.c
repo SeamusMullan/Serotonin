4ad25a7b6a9b11f8719196ee595fde115134de94
balazs <113060109+balazsmonor1@users.noreply.github.com>
2025-06-25 16:31:15 +0100

    version bump: 0.1.0

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -21,8 +21,8 @@
 #include "audio/pcspeaker/pcspeaker.h"
 
 #define KERNEL_VERSION_HIGH 0
-#define KERNEL_VERSION_MID 0
-#define KERNEL_VERSION_LOW 3 
+#define KERNEL_VERSION_MID 1
+#define KERNEL_VERSION_LOW 0
 
 #define HEAP_START  ((uint8_t*) (KERNEL_HEAP_VMA))
 #define HEAP_SIZE   (KERNEL_HEAP_SIZE)
