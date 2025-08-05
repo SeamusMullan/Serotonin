4a63821832c303bd337f4d2b094bf69cd9a88fae
balazs <113060109+balazsmonor1@users.noreply.github.com>
2025-08-05 22:01:29 +0100

    rtc fix century

--- kernel/io/irq.c
+++ kernel/io/irq.c
@@ -59,7 +59,7 @@ void irq_handler(int irq, processor_context_t *ctx) {
             t.year   = bcd_to_bin(t.year);
         }
 
-        t.century = 2000; // sure look its fine until 2099
+        t.century = 20; // sure look its fine until 2099
 
         if (!hour_24) {
             uint8_t pm = t.hour & 0x80;
@@ -68,7 +68,7 @@ void irq_handler(int irq, processor_context_t *ctx) {
             else if (!pm && t.hour == 12) t.hour = 0;
         }
 
-        printf("time: %d:%d:%d %d/%d/%d \n", t.hour,t.minute,t.second,t.day,t.month,t.year+t.century);
+        printf("time: %d:%d:%d %d/%d/%d%d \n", t.hour,t.minute,t.second,t.day,t.month,t.century,t.year);
     }
 
 end_irq:
