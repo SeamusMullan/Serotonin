1617e1baa1a9138fd9b477ab511e219c74442561
balazs <113060109+balazsmonor1@users.noreply.github.com>
2025-10-28 00:59:40 +0000

    redo init

--- build/build-user.sh
+++ build/build-user.sh
@@ -11,11 +11,11 @@ cd ../user
 
 i686-elf-as crt0.s -o crt0.o
 
-i686-elf-gcc -c test.c -o test.o
+i686-elf-gcc -c init/init.c -o init/init.o
 i686-elf-gcc -c syscall.c -o syscall.o
 i686-elf-gcc -c shell.c -o shell.o
 i686-elf-gcc -c games/sponk/sponk.c -o games/sponk/sponk.o
 
-i686-elf-gcc -Ttext=0x400100 -nostdlib crt0.o test.o syscall.o -Wl,--start-group -lc -lm -Wl,--end-group -o test.elf
-i686-elf-gcc -Ttext=0x400100 -nostdlib crt0.o shell.o syscall.o -Wl,--start-group -lc -lm -Wl,--end-group -o shell.elf
+i686-elf-gcc -Ttext=0x400100 -nostdlib crt0.o init/init.o syscall.o -Wl,--start-group -lc -lm -Wl,--end-group -o init.elf
+i686-elf-gcc -Ttext=0x400100 -nostdlib crt0.o shell.o syscall.o -Wl,--start-group -lc -lm -Wl,--end-group -o sh.elf
 i686-elf-gcc -Ttext=0x400100 -nostdlib crt0.o games/sponk/sponk.o syscall.o -Wl,--start-group -lc -lm -Wl,--end-group -o sponk.elf
--- user/init/init.c
+++ user/init/init.c
@@ -1,46 +1,13 @@
-#include <stdint.h>
-#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
 
-/**
- * @brief Makes a system call.
- *
- * @param arg1 The first argument.
- * @param arg2 The second argument.
- * @param arg3 The third argument.
- * @param arg4 The fourth argument.
- * @return uint32_t The return value of the system call.
- */
-uint32_t system_call(uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4) {
-    uint32_t ret;
-    asm volatile (
-        "int $0x80"
-        : "=a" (ret)
-        : "a" (arg1),  // eax
-          "b" (arg2),  // ebx
-          "c" (arg3),  // ecx
-          "d" (arg4)   // edx
-        : "memory"
-    );
-    return ret;
-}
-
-/**
- * @brief The entry point of the user program.
- */
 int main(int argc, char **argv, char **envp) {
-    char* strdbg = "Serotonin usermode init, loading unit test (/bin/test)\n";
-    system_call(1,0,(uint32_t)strdbg,0);
+    printf("Welcome to Serotonin from init!\n");
+    printf("Loading shell\n");
 
-    char lol4[] = "/bin/test";
-    uint32_t pid = system_call(4,0,0,0);
-    if (pid == 0) {
-        char *argv[] = {"/bin/test", "arg1", "arg2", NULL};
-        char *envp[] = {"PATH=/", NULL};
-        system_call(3,(uint32_t)lol4,(uint32_t)argv,(uint32_t)envp);
-    } else {
-        int troll = 0;
-        system_call(9,pid,(uint32_t)&troll,0);
-    }
+    execve("/bin/sh", argv, envp);
+    
+    printf("Failed to load shell!\n");
 
-    return 0;
+    return 1;
 }
