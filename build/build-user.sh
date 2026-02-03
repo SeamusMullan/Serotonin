9cb750be32d29d2f610863e9f2c2cabbd2bdb432
balazs <113060109+balazsmonor1@users.noreply.github.com>
2026-02-03 07:13:06 +0000

    devfs test (not working yet)

--- build/build-user.sh
+++ build/build-user.sh
@@ -31,6 +31,7 @@ i686-elf-gcc -c fb_layer_test.c -o fb_layer_test.o $CFLAGS
 i686-elf-gcc -c receiver.c -o receiver.o $CFLAGS
 i686-elf-gcc -c sender.c -o sender.o $CFLAGS
 i686-elf-gcc -c fs_syscall_test.c -o fs_syscall_test.o $CFLAGS
+i686-elf-gcc -c devfs_example.c -o devfs_example.o $CFLAGS
 i686-elf-gcc -c listproc/listproc.c -o listproc/listproc.o $CFLAGS
 i686-elf-gcc -c games/sponk/sponk.c -o games/sponk/sponk.o $CFLAGS
 
@@ -45,6 +46,7 @@ i686-elf-gcc -Ttext=0x400100 -nostdlib cxx/cxx_init.o cxx/cxx_new_delete.o cxx/c
 i686-elf-gcc -Ttext=0x400100 -nostdlib cxx/cxx_init.o cxx/cxx_new_delete.o cxx/cxx_runtime.o crt0.o games/sponk/sponk.o syscall/syscall.o syscall/lib5ht/lib5ht.o -Wl,--start-group -lc -lm -Wl,--end-group -o sponk.elf
 i686-elf-gcc -Ttext=0x400100 -nostdlib cxx/cxx_init.o cxx/cxx_new_delete.o cxx/cxx_runtime.o crt0.o receiver.o syscall/syscall.o syscall/lib5ht/lib5ht.o -Wl,--start-group -lc -lm -Wl,--end-group -o receiver.elf
 i686-elf-gcc -Ttext=0x400100 -nostdlib cxx/cxx_init.o cxx/cxx_new_delete.o cxx/cxx_runtime.o crt0.o sender.o syscall/syscall.o syscall/lib5ht/lib5ht.o -Wl,--start-group -lc -lm -Wl,--end-group -o sender.elf
+i686-elf-gcc -Ttext=0x400100 -nostdlib cxx/cxx_init.o cxx/cxx_new_delete.o cxx/cxx_runtime.o crt0.o devfs_example.o syscall/syscall.o syscall/lib5ht/lib5ht.o -Wl,--start-group -lc -lm -Wl,--end-group -o devfs_example.elf
 
 i686-elf-g++ -c cxx_test.cpp -o cxx_test.o $CXXFLAGS
 i686-elf-g++ -Ttext=0x400100 -nostdlib cxx/cxx_init.o cxx/cxx_new_delete.o cxx/cxx_runtime.o crt0.o cxx_test.o syscall/syscall.o syscall/lib5ht/lib5ht.o -Wl,--start-group -lc -lm -Wl,--end-group -o cxxtest.elf
---
+++ user/devfs_example.c
@@ -0,0 +1,48 @@
+/*
+ * devfs_example.c
+ * Simple devfs client to read /dev/example/number and write /dev/example/write.
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void) {
+    char buf[32];
+    int fd = open("/dev/example/number", O_RDONLY);
+    printf("/dev/example/number fd:%d\n",fd);
+    if (fd < 0) {
+        printf("open number failed fd=%d errno=%d\n", fd, errno);
+        return 1;
+    }
+
+    int n = read(fd, buf, sizeof(buf) - 1);
+    if (n < 0) {
+        printf("read number failed errno=%d\n", errno);
+        close(fd);
+        return 1;
+    }
+    buf[n] = '\0';
+    printf("number read: %s", buf);
+    close(fd);
+
+    fd = open("/dev/example/write", O_WRONLY);
+    printf("/dev/example/write fd:%d\n",fd);
+    if (fd < 0) {
+        printf("open write failed fd=%d errno=%d\n", fd, errno);
+        return 1;
+    }
+
+    const char *msg = "hello from user\n";
+    int w = write(fd, msg, (int)strlen(msg));
+    if (w < 0) {
+        printf("write failed errno=%d\n", errno);
+        close(fd);
+        return 1;
+    }
+    close(fd);
+
+    return 0;
+}
